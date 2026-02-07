d46fd1fefbd8efbf8b36b5332fff90555d7aeaa1
Bruno Meilick <b@bnomei.com>
2026-02-07 23:54:05 +0000

    inital commit

---
+++ Formula/raymon.rb
@@ -0,0 +1,43 @@
+class TmuxMcpRs < Formula
+  desc "Raymon"
+  homepage "https://github.com/bnomei/raymon"
+  version "0.1.0"
+  license "MIT"
+
+  checksums = {
+    aarch64_apple_darwin: "9614e53ca665684ab64c831d75ed3421d4acdff369b94c2ab2b2517d8c4f68ee",
+    x86_64_apple_darwin: "1e13a9308bd94f5992e8dae5dad009693a878bc2f68514abab3a135dda7926e9",
+    aarch64_unknown_linux_musl: "b4910f5e4caedc70c9f288b228426fc8765f9a2f73d8916b5723a226dab7677e",
+    x86_64_unknown_linux_musl: "07c0294fe5a460412a85290e3666bc2829d769f342447ac765b0a525d9d97529",
+  }
+
+  on_macos do
+    on_arm do
+      url "https://github.com/bnomei/raymon/releases/download/v#{version}/raymon-rs-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 checksums[:aarch64_apple_darwin]
+    end
+    on_intel do
+      url "https://github.com/bnomei/raymon/releases/download/v#{version}/raymon-rs-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 checksums[:x86_64_apple_darwin]
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/bnomei/raymon/releases/download/v#{version}/raymon-rs-v#{version}-aarch64-unknown-linux-musl.tar.gz"
+      sha256 checksums[:aarch64_unknown_linux_musl]
+    end
+    on_intel do
+      url "https://github.com/bnomei/raymon/releases/download/v#{version}/raymon-rs-v#{version}-x86_64-unknown-linux-musl.tar.gz"
+      sha256 checksums[:x86_64_unknown_linux_musl]
+    end
+  end
+
+  def install
+    bin.install "raymon"
+  end
+
+  test do
+    assert_match "raymon", shell_output("#{bin}/raymon --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# homebrew-raymon
+
+Homebrew tap for raymon.
+
+## Install
+
+```bash
+brew install bnomei/raymon/raymon
+```
+
+## Maintain
+
+Bump `version` and update all `sha256` values in `Formula/raymon.rb` to match the
+GitHub Release tarballs from `bnomei/raymon`.
+
+```bash
+scripts/update-sha256.sh
+```
