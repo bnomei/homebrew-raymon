d0ba7f2102ed444ce14089a8530d0a3c96482593
Bruno Meilick <b@bnomei.com>
2026-02-08 01:28:39 +0000

    :bookmark: v0.1.0

--- Formula/raymon.rb
+++ Formula/raymon.rb
@@ -5,10 +5,10 @@ class TmuxMcpRs < Formula
   license "MIT"
 
   checksums = {
-    aarch64_apple_darwin: "9614e53ca665684ab64c831d75ed3421d4acdff369b94c2ab2b2517d8c4f68ee",
-    x86_64_apple_darwin: "1e13a9308bd94f5992e8dae5dad009693a878bc2f68514abab3a135dda7926e9",
-    aarch64_unknown_linux_musl: "b4910f5e4caedc70c9f288b228426fc8765f9a2f73d8916b5723a226dab7677e",
-    x86_64_unknown_linux_musl: "07c0294fe5a460412a85290e3666bc2829d769f342447ac765b0a525d9d97529",
+    aarch64_apple_darwin: "d0abba0d9cbc71ed6953b37148671930e20fe4926bdf9e5b7dde23ec82e5a4bd",
+    x86_64_apple_darwin: "83930c53106e1c147cc496ae38c8c119cae6c956633692d7aae9c8051b537a1a",
+    aarch64_unknown_linux_musl: "d50623db36f9e9ab7f8f663c41723479e505473943c1383e6aa2c6c4a5584ba8",
+    x86_64_unknown_linux_musl: "3220f738924835bed568869b9e8716d3fbf57419c3a7e4e48c734bf53aefb5cf",
   }
 
   on_macos do
